8a4c99a86434c05aa20a699552788cb3e221367c
fawzywahba15 <121544643+fawzywahba15@users.noreply.github.com>
2023-01-05 00:29:25 +0100

    admin shit done

--- admin/update_data.php
+++ admin/update_data.php
@@ -3,24 +3,23 @@ $userid = $_POST["id"];
 $username = $_POST["username"];
 $first_name = $_POST["first_name"];
 $usermail = $_POST["usermail"];
-
 $password = $_POST["password"];
+$hashed_pw = password_hash($password, PASSWORD_DEFAULT);
 
 if (empty($password)){
     // Connect to the database and update the data
     $conn = mysqli_connect("localhost", "fawzy", "mypassword", "regestrieren");
     $sql = "UPDATE login SET username = '$username', `first_name` = '$first_name', usermail = '$usermail' WHERE id = '$userid'";
-    mysqli_query($conn, $sql);
+    $result = mysqli_query($conn, $sql);
 
-// Close the connection
-    mysqli_close($conn);
+
 
 
 }else{
     // Connect to the database and update the data
     //$new_password = hash($password,PASSWORD_DEFAULT);
     $conn = mysqli_connect("localhost", "fawzy", "mypassword", "regestrieren");
-    $sql = "UPDATE login SET username = '$username', `first_name` = '$first_name', usermail = '$usermail', password = '$password' WHERE id = '$userid'";
+    $sql = "UPDATE login SET username = '$username', `first_name` = '$first_name', usermail = '$usermail', password = '$hashed_pw' WHERE id = '$userid'";
     mysqli_query($conn, $sql);
 
 // Close the connection
