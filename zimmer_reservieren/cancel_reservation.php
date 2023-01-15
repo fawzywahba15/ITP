d9b34a235f9c35fa1c6a32e6196906e6a2e126f1
fawzywahba15 <121544643+fawzywahba15@users.noreply.github.com>
2023-01-15 21:30:51 +0100

    Update cancel_reservation.php

--- zimmer_reservieren/cancel_reservation.php
+++ zimmer_reservieren/cancel_reservation.php
@@ -1,25 +1,17 @@
 <?php
 
 // Connect to the database
-$conn = mysqli_connect("localhost", "fawzy", "mypassword", "regestrieren");
+include_once "../0include/dbaccess.php";
 
 // Check if the 'id' parameter was passed to the script
 if (isset($_POST['id'])) {
     $buchungsnummer = $_POST['id'];
-    // Delete the reservation with the specified id from the database
-    //"update `login` Set `$spalten_name` = '$data_to_change' where `username` = '$current_user' ";
     $sql = "update reservierungen Set status = 'storniert'  WHERE id='$buchungsnummer'";
-    $result = mysqli_query($conn, $sql);
-    if ($result) {
-        // The delete was successful, return a success message to the client
-        echo "success";
-    } else {
-        // The delete was not successful, return an error message to the client
-        echo "error";
-    }
+    $result = mysqli_query($db_obj, $sql);
+
 }
 
 // Close the database connection
-mysqli_close($conn);
+mysqli_close($db_obj);
 
 ?>
