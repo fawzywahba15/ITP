1e9f26ad3bc3d025ab0829bc003cbb9bb68e8042
fawzywahba15 <121544643+fawzywahba15@users.noreply.github.com>
2023-06-11 20:20:51 +0200

    d

--- admin/change_verkauf.php
+++ admin/change_verkauf.php
@@ -2,16 +2,22 @@
 
 include_once "../0include/dbaccess.php";
 
-if (isset($_POST['id'])) {
+if (isset($_POST['id'], $_POST['status'])) {
     $id = $_POST['id'];
+    $status = $_POST['status'];
 
-    $status = $_POST["status"];
-    $sql = "UPDATE bestellung SET `status` = '$status' WHERE id = '$id'";
+    $sql = "UPDATE bestellungen SET `status` = ? WHERE `id` = ?";
+    $stmt = mysqli_prepare($db_obj, $sql);
 
-    $result = mysqli_query($db_obj, $sql);
+
+    mysqli_stmt_bind_param($stmt, "si", $status, $id);
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
 
 }
 
 mysqli_close($db_obj);
+
 
 ?>
