36a03101e7df58919bfbd16bd7cf2c890a8551bc
fawzywahba15 <121544643+fawzywahba15@users.noreply.github.com>
2023-06-19 16:55:16 +0200

    s

--- 0include/footer.php
+++ 0include/footer.php
@@ -3,6 +3,6 @@
 <div class="container mx-3 my-5 d-block">
     <h3>Kontaktdaten:</h3>
     Telefonnummer: +436767029823 <br>
-    E-mail: <a href="mailto: info@Viennapalace.at" class="mail">info@viennapalace.at</a> <br> <br>
+    E-mail: <a href="mailto: info@Sneakerstore.at" class="mail">info@Sneakerstore.at</a> <br> <br>
 </div>
 
